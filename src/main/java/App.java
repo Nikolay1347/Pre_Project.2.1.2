b849714cc59542ac7eee1a87b32df31ded668df0
Nikolay1347 <kolyan29041985@gmail.com>
2025-03-26 15:43:32 +0300

    v1.1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,18 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean == bean2);
+
+        Cat cat_bean = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(cat_bean.getNickname());
+
+        Cat cat_bean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat_bean == cat_bean2);
+
+
     }
 }
